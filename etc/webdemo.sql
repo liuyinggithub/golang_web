93a4451011176c5d58a5b4d49b333a33135b17dd
liuying-c <liuying-c@360.cn>
2016-07-23 11:43:21 +0800

    初始化数据库信息

--- etc/webdemo.sql
+++ etc/webdemo.sql
@@ -1,7 +1,13 @@
-create table webdemo_admin
-(
-    admin_id int not null auto_increment,
-    admin_name varchar(32) not null,
-    admin_password varchar(32) not null,
-    primary key(admin_id)
-);
+create database webdemo;
+use webdemo;
+/*
+创建表
+*/
+CREATE TABLE `webdemo_admin` (
+  `admin_id` int(11) NOT NULL AUTO_INCREMENT,
+  `admin_name` varchar(32) NOT NULL,
+  `admin_password` varchar(32) NOT NULL,
+  PRIMARY KEY (`admin_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8
+/*插入管理员账号密码*/
+insert into admin values(null, 'liuying', 'liuying');
